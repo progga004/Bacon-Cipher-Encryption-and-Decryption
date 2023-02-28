5942f3a87fe3eafbe45da2a4e41b290fdf60180e
Progga Paromita Dutta <proggaparomita.dutta@stonybrook.edu>
2023-02-27 23:56:40 -0500

    third load

--- src/bacon.c
+++ src/bacon.c
@@ -1,6 +1,7 @@
 #include "bacon.h"
 #include <string.h>
 #include <stdio.h>
+#include<ctype.h>
 // Add other #includes here if you want.
 
 int encrypt(const char *plaintext, char *ciphertext)
@@ -35,31 +36,21 @@ int encrypt(const char *plaintext, char *ciphertext)
     char str2[length_2];
     char str3[length_1];
     int k = 0;
+    int n=0;
     // for another pointer to point out the new string
-    for ( int n = 0; ciphertext[n] != '\0'; n++)
+    for (  n = 0; ciphertext[n] != '\0'; n++)
     {
-        if (ciphertext[n] >= 'A' && ciphertext[n] <= 'Z')
-        {
-            str2[n] = ciphertext[n] + 32;
-        }
-        else
-        {
-            str2[n] = ciphertext[n];
-        }
+       
+        str2[n]=tolower(ciphertext[n]);
     }
-    //str2[i]='\0'; // creating all lower case letters string named str2 from ciphertext
-     for ( int m = 0; plaintext[m] != '\0'; m++)
+    str2[n]='\0'; // creating all lower case letters string named str2 from ciphertext
+    int m=0;
+     for (  m = 0; plaintext[m] != '\0'; m++)
     {
-        if (plaintext[m] >= 'a' && ciphertext[m] <= 'z')
-        {
-            str3[m] = plaintext[m]- 32;
-        }
-        else
-        {
-            str3[m] = plaintext[m];
-        }
+        
+        str3[m]=toupper(plaintext[m]);
     }
-   // str3[i]='\0'; // creating all uppercase letters string named str3 from plaintext
+    str3[m]='\0'; // creating all uppercase letters string named str3 from plaintext
     for (int i = 0; i < words; i++)
     { char *ptr; // creating a pointer to store the specific character value 6 bits from plaintext
        if (str3[i] >= 'A' && str3[i] <= 'Z')
