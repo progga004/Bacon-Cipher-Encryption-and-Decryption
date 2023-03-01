afbd52d765cafc14dfa9fdb6440ab7926f24aaf4
Progga Paromita Dutta <proggaparomita.dutta@stonybrook.edu>
2023-03-01 02:42:14 -0500

    Sixth load

--- src/bacon.c
+++ src/bacon.c
@@ -2,7 +2,7 @@
 #include "bacon.h"
 #include <string.h>
 #include <stdio.h>
-#include<ctype.h>
+//#include<ctype.h>
 // Add other #includes here if you want.
 
 int encrypt(const char *plaintext, char *ciphertext)
@@ -42,17 +42,19 @@ int encrypt(const char *plaintext, char *ciphertext)
     for (  n = 0; ciphertext[n] != '\0'; n++)
     {
        
-       ciphertext[n]=tolower(ciphertext[n]);
+        str2[n]=ciphertext[n];
     }
-    //str2[n]='\0'; // creating all lower case letters string named str2 from ciphertext
-    for (  n = 0; str2[n] != '\0'; n++)
+    str2[n]!='\0';
+    for (  n = 0; ciphertext[n] != '\0'; n++)
     {
-       
-        str2[n]=tolower(str2[n]);
-    }
-    //
-    //str2[n]='\0';
+       if(ciphertext[n]>='A'&& ciphertext[n]<='Z')
 
+       {ciphertext[n]=(ciphertext[n]+32);}
+       else
+       {
+        ciphertext[n]=ciphertext[n];
+       }
+    }
     int m=0;
      for (  m = 0; plaintext[m] != '\0'; m++)
     {
@@ -63,7 +65,13 @@ int encrypt(const char *plaintext, char *ciphertext)
     for (  m = 0; str3[m] != '\0'; m++)
     {
         
-        str3[m]=toupper(str3[m]);
+       if(str3[m]>='a'&& str3[m]<='z')
+
+       {str3[m]=(str3[m]-32);}
+       else
+       {
+        str3[m]=str3[m];
+       }
     }
     for (int i = 0; i < words; i++)
     { char *ptr; // creating a pointer to store the specific character value 6 bits from plaintext
@@ -153,29 +161,160 @@ int encrypt(const char *plaintext, char *ciphertext)
                
             
         }
-        ciphertext[k]=ciphertext[cipertextpos];// if there is still something to count just put them
+        ciphertext[k]=str2[cipertextpos];// if there is still something to count just put them
         cipertextpos++;
          k++;
          
     }
      
-     //str1[k]='\0'; // the final encrypted cipher text
-     //printf("the ciphertext is %s\n",str1);
-     //ciphertext=str1;
-     //printf("the ciphertext is %s\n",ciphertext);
+     
      return words;// return the words encrypted from plaintext
 }
 
 int decrypt(const char *ciphertext, char *plaintext)
 {
-    return -1000;
+    char bconcodes[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ','!','"',
+      '#','$','%','&','\'','(',')',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',':','?'};
+     int length_1 = strlen(plaintext); //length of the plaintext
+     int length_2 = strlen(ciphertext); // length of the cipher text
+     int countWithoutSpaces = 0;
+     char str1[length_2];
+     int i,j=0;
+     for (i = 0; ciphertext[i] != '\0'; i++) // creating the countwithout space
+    {
+        if (ciphertext[i]>=' '&& ciphertext[i]<='/'|| ciphertext[i]>='0'&& ciphertext[i]<='9')
+        {
+            countWithoutSpaces = countWithoutSpaces + 1;
+        }
+        else
+        {
+            str1[j]=ciphertext[i];
+            j++;
+        }
+       
+    }
+     
+    str1[j]='\0';
+    int index,count,cipertextpos,plaintextpos =0;
+  if(strlen(ciphertext)>=6){
+   for(int p=0;p<strlen(str1);p=p+6)
+   {
+       int ptr[6];
+       int j=0;
+       int sum=0;
+       while(j<=5)
+       {
+           if(str1[cipertextpos]>='A' && str1[cipertextpos]<='Z')
+           {
+               ptr[j]=1;
+               j++;
+               cipertextpos++;
+           }
+           else
+           {
+               ptr[j]=0;
+               j++;
+               cipertextpos++;
+           }
+         
+       }
+      
+       int new_one=32;
+       for(int k=0;k<6;k++)
+      {
+          sum=sum+ptr[k]*new_one;
+          new_one=new_one/2;
+      }
+    
+      index=sum;
+      if(index==63 )
+      {
+          break;
+      }
+      
+      else{
+      if(index>=0 && index<=52)
+      {
+       char c= bconcodes[index];
+       plaintext[plaintextpos]=c;
+        plaintextpos++;
+          count++;
+      }
+      else
+      {
+          return -3;
+         
+      }
+      }
+      
+    }
+    
+    plaintext[plaintextpos]='\0';
+    }
+    if(strlen(plaintext)==0 && strlen(ciphertext)>=6)
+    {
+        plaintext[plaintextpos]='\0';
+        return -1;
+    }
+    
+    if(strlen(ciphertext)<6)
+    {
+               plaintext[plaintextpos]='\0';
+               return -2;
+                
+    }
+    
+    return count; 
 }
 // int main()
 // {
-//     char ciphertext[] = "I can store three characters! Yes!";
+//     char ciphertext_act[] = "One of the main causes of the fall of the Roman Empire was that lacking zero, they had no way to indicate successful termination of their C programs.";
+//     char *plaintext = "--Robert Firth";
+//     int count_act = encrypt(plaintext, ciphertext_act);
+//     char *ciphertext_exp = "One Of THe mAiN cAuseS of THE fall of The rOman EmpiRe Was THaT LaCking ZeRo, tHey haD no wAy To iNDicaTE SUCCESSful termination of their C programs.";
+//      int count_exp = 14;
+//    printf( "ciphertext was:          %s\nbut it should have been: %s", ciphertext_act, ciphertext_exp);
+//    printf("Return value was %d, but it should have been %d.\n", count_act, count_exp);
+//    char ciphertext_act[] = "Too Short 2022";
 //     char *plaintext = "Stony Brook University";
-//     int count_act = encrypt(plaintext, ciphertext);  // s = 010010, t = 010011, o = 001110
-//     char *ciphertext_exp = ciphertext;
-//     printf("%s %d the new one\n",ciphertext_exp,count_act);
+//     int count_act = encrypt(plaintext, ciphertext_act);
+//     char *ciphertext_exp = "TOO SHOrt 2022";
+//     int count_exp = 0;
+//    printf("ciphertext was:          %s\nbut it should have been: %s", ciphertext_act, ciphertext_exp);
+//     printf("Return value was %d, but it should have been %d.\n", count_act, count_exp);
+// char ciphertext_act[] = "2023 United States of America";
+//     char *plaintext = "";
+//     int count_act = encrypt(plaintext, ciphertext_act);
+//     char *ciphertext_exp = "2023 UNITED States of America";
+//     int count_exp = 0;
+//     printf("ciphertext was:          %s\nbut it should have been: %s", ciphertext_act, ciphertext_exp);
+//     printf("Return value was %d, but it should have been %d.\n", count_act, count_exp);
+// char ciphertext_act[] = "I can store three characters! Yes!";
+//     char *plaintext = "Stony Brook University";
+//     int count_act = encrypt(plaintext, ciphertext_act);  // s = 010010, t = 010011, o = 001110
+//     char *ciphertext_exp = "i Can StoRe tHRee CHArACTERS! Yes!";
+//     int count_exp = 3;
+//    printf( "ciphertext was:          %s\nbut it should have been: %s", ciphertext_act, ciphertext_exp);
+//     printf( "Return value was %d, but it should have been %d.\n", count_act, count_exp);
+// // char plaintext_act[] = "*******************";
+// //     for (unsigned int i = 0; i < strlen(plaintext_act); i++)
+// //         plaintext_act[i] = (char)(rand() % 200 + 33);
+// //     char *ciphertext = "One Of THe mAiN cAuseS of THE fall of The rOman EmpiRe Was THaT LaCking ZeRo, tHey haD no wAy To iNDicaTE SUCCESSful termination of their C programs.";
+// //     int count_act = decrypt(ciphertext, plaintext_act);  
+// //     char *plaintext_exp = "--ROBERT FIRTH";
+// //     int count_exp = 14;
+// //     printf("plaintext was:           %s\nbut it should have been: %s", plaintext_act, plaintext_exp);
+// //     printf("Return value was %d, but it should have been %d.\n", count_act, count_exp);
+// // char plaintext_act[] = "@@@@@@@@@";
+// //     for (unsigned int i = 0; i < strlen(plaintext_act); i++)
+// //         plaintext_act[i] = (char)(rand() % 200 + 33);
+// //     char *ciphertext = "i Can StoRe tHRee CHArACTERS! Yes!";
+// //     int count_act = decrypt(ciphertext, plaintext_act);  
+// //     char *plaintext_exp = "STO";
+// //     int count_exp = 3;
+// // printf( "plaintext was:           %s\nbut it should have been: %s", plaintext_act, plaintext_exp);
+// //   printf("Return value was %d, but it should have been %d.\n", count_act, count_exp);
+
+
 //     return 0;
 // }
